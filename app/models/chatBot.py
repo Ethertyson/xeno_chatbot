48db81e1f8b0863ec1a936ef2e2c3cd89f2f4d36
Pritanshu Srivastava <pritanshusrivastava24880@gmail.com>
2025-06-05 16:54:01 +0530

    Updated chatbot logic, added numexpr,pytz improved expression & bitwise handling

--- app/models/chatBot.py
+++ app/models/chatBot.py
@@ -2,18 +2,44 @@
 
 from flask import jsonify
 import re
-from app.utils.makeReply import fetchBestReply,fetchBasicReply,numberClaculationReply
+from app.utils.makeReply import fetchBestReply,fetchBasicReply,numberClaculationReply,expressionCalculationReply
 from word2number import w2n
 class startChat:
 
-    arithmeticKeywords = [
+    notOperatorKeywords = ['bitwise not', 'bitwise ~','bitwisenot', 'bitwise~','~']
+    semanticOperatorKeywords = [
+    # Addition   
     'add','added', 'addition', '+', 'plus',
+
+    # Subtraction
     'sub', 'subtract','subtracted', 'subtraction', '-', 'minus',
+
+    # Multiplication
     'multiply','multiplied', 'multiplication', '*', 'times',
+
+    # Division
     'divide', 'divided', 'division', '/','quotient','by',
+
+    # Exponentiation
     'power', 'exponent', '**',
-    'mod', 'modulo', 'remainder', '%'
+
+    # Modulo
+    'mod', 'modulo', 'remainder', '%',
+
+    # Bitwise AND
+    'bitwise and', 'bitwise &','bitwiseand', 'bitwise&','bitwise',
+
+    # Bitwise XOR
+    'bitwise xor', 'bitwise ^','bitwisexor', 'bitwise^','xor','^',
+
+    # Left Shift
+    'left shift', '< <', '<<','leftshift','bitwise left shift','bitwise <<','bitwiseleftshift','bitwise<<','bitwise leftshift','bitwiseleft shift','bitwise < <','bitwise< <',
+
+    # Right Shift
+    'right shift', '> >', '>>','rightshift','bitwise right shift','bitwise >>','bitwiserightshift','bitwise>>','bitwise rightshift','bitwiseright shift','bitwise > >','bitwise> >'
     ]
+
+    symbolOperatorKeywords = ['+','-','*','/','**','%','<','<=','>','>=','==','!=']
 
     @staticmethod
     def chatBotResponseByTopic(postRequestData):
@@ -35,9 +61,25 @@ class startChat:
                 
                 user_msg2 = " ".join(user_msg2)
 
-                if any(i in user_msg2 for i in startChat.arithmeticKeywords) and bool(re.search(r'\d+',user_msg2)) and len(re.findall(r'\d+',user_msg2)) > 1:
-                    calculationReply = numberClaculationReply(user_msg2)
-                    response = {"Status":"Success","Message":f"The result is {calculationReply}. If the result is unexpected, try to use '-' between the numbers > 20 if you're writing number in words. Then, I'll try to give you the expected result."}
+                if bool(re.search(r'\d+',user_msg2)) and len(re.findall(r'\d+',user_msg2)) > 1:
+
+                    calculationReply = 0
+
+                    if len(list(filter(lambda q: q in user_msg2,startChat.symbolOperatorKeywords))) > 1:
+                        calculationReply = expressionCalculationReply(user_msg2)
+
+                    elif any(i in user_msg2 for i in startChat.semanticOperatorKeywords):
+                        calculationReply = numberClaculationReply(user_msg2)
+
+                    response = {"Status":"Success","Message":f"{calculationReply}.\nIf the result is unexpected, try to use '-' between the numbers > 20 if you're writing number in words."}
+
+                elif (any(i in user_msg2 for i in startChat.notOperatorKeywords) or ('bitwise' in user_msg2 and 'not' in user_msg2)) and len(re.findall(r'\d+',user_msg2)) == 1:
+
+                    calculationReply = 0
+
+                    calculationReply = numberClaculationReply(user_msg2,'NOT')
+                    response = {"Status":"Success","Message":f"{calculationReply}.\nIf the result is unexpected, try to use '-' between the numbers > 20 if you're writing number in words."}
+
                 else:
                     basicReplyAndScore = fetchBasicReply(user_msg)
                     basicReplyScore,basicReply = basicReplyAndScore[0],basicReplyAndScore[1]
@@ -53,7 +95,7 @@ class startChat:
                         response = {"Status":"Success","Message":botSemanticBasedReply}
             else:
                 # response = {"Status":"Success","Message":user_msg}
-                response = {"Status":"Success","Message":"Hey, sorry I didn't got your query, this is Xeno Version1. You can ask me different topics like Windows commands, Linux commands, macOS commands, PowerShell, GitHub commands, MySQL queries, computer science concepts, jokes, random facts, some basic arithmetic calculations, or even the latest tech news. I'd love to help where I can!"}
+                response = {"Status":"Success","Message":"Hey, sorry I didn't got your query, this is Xeno Version1.\nYou can ask me different topics like Windows commands, Linux commands, macOS commands, PowerShell, GitHub commands, MySQL queries, computer science concepts, jokes, random facts, and mysteries.\nAlso I can do some basic calculations, or find the latest news for you.\nI'd love to help where I can!"}
 
         except Exception as error:
             response = {"Status":"Failed","Message":f"Sorry, I'm getting error in generating response: {error}"}
--- app/utils/makeReply.py
+++ app/utils/makeReply.py
@@ -6,6 +6,8 @@ from app.utils.findNews import fetchLatestNews
 from datetime import datetime,date
 import re
 from functools import reduce
+import numexpr as ner
+import pytz
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
@@ -22,7 +24,7 @@ def fetchBestReply(userInput):
 
     score = matchScores[0][bestMatchIntIndex].item()
     if score < 0.5:  # adjust this as needed
-        return "Sorry, I couldn't assist with that topic. I'm a lightweight AI style chatbot, not a fully developed AI tool! But feel free to ask me about things like Windows commands, Linux commands, macOS commands, PowerShell, GitHub commands, MySQL queries, computer science concepts, jokes, random facts, some basic arithmetic calculations, or even the latest tech news. I'd love to help where I can!"
+        return "Sorry, I couldn't assist with that topic.\nI'm a lightweight AI style chatbot, not a fully developed AI tool! But feel free to ask me about things like Windows commands, Linux commands, macOS commands, PowerShell, GitHub commands, MySQL queries, computer science concepts, jokes, random facts, and mysteries.\nI can perform some calculations, or even find the latest news for you.\nI'd love to help where I can!"
 
 
     bestMatchCommand = allTopicCommandsDict[allCmdSemanticKeys[bestMatchIntIndex]]
@@ -30,11 +32,11 @@ def fetchBestReply(userInput):
     if bestMatchCommand == 'FETCH LATEST NEWS':
         bestMatchCommand = fetchLatestNews()
     elif bestMatchCommand == "GET CURRENT TIME":
-        bestMatchCommand = datetime.now().time().strftime("%H:%M:%S")
+        bestMatchCommand = f"Current time in India is {datetime.now(pytz.timezone('Asia/Kolkata')).time().strftime('%H:%M:%S')}"
     elif bestMatchCommand == "GET CURRENT DATE":
-        bestMatchCommand = date.today().strftime("%Y-%m-%d")
+        bestMatchCommand = f"Today's date in India is {datetime.now(pytz.timezone('Asia/Kolkata')).date().strftime('%Y-%m-%d')}"
     elif bestMatchCommand == "GET TIME AND DATE":
-        bestMatchCommand = datetime.now().strftime("%H:%M:%S %Y-%m-%d")
+        bestMatchCommand = f"Current date and time in India is {datetime.now(pytz.timezone('Asia/Kolkata')).strftime('%H:%M:%S %Y-%m-%d')}"
 
     return bestMatchCommand
 
@@ -52,30 +54,52 @@ def fetchBasicReply(userInput):
     
     return score,initialCommandsDict[basicCommandKeys[bestMatchIntIndex]]
 
-def numberClaculationReply(userInput):
-
+def numberClaculationReply(userInput,operator=None):
+    
     calculatedResult = 0
     numberList = re.findall(r'\d+', userInput)
 
-    if 'add' in userInput or 'added' in userInput or 'addition' in userInput or '+' in userInput or 'plus' in userInput:
-        calculatedResult = sum(list(map(int,numberList)))
+    if operator == 'NOT':
+        calculatedResult = f"The bitwise ~ of {numberList[0]} is {~int(numberList[0])}"
 
-    elif 'multiply' in userInput or 'multiplied' in userInput or 'multiplication' in userInput or '*' in userInput or 'times' in userInput:
-        calculatedResult = reduce(lambda a,b: a*b,list(map(int,numberList)))
+    else:
+        if 'add' in userInput or 'added' in userInput or 'addition' in userInput or '+' in userInput or 'plus' in userInput:
+            calculatedResult = f"Addtion of numbers is {sum(list(map(int,numberList)))}"
 
-    elif 'sub' in userInput or 'subtract' in userInput or 'subtracted' in userInput or 'subtraction' in userInput or 'minus' in userInput or '-' in userInput:
-        if 'from' in userInput:
-            calculatedResult = int(numberList[1]) - int(numberList[0])
-        else:
-            calculatedResult = int(numberList[0]) - int(numberList[1])
+        elif 'multiply' in userInput or 'multiplied' in userInput or 'multiplication' in userInput or '*' in userInput or 'times' in userInput:
+            calculatedResult = f"Multiplication of numbers is {reduce(lambda a,b: a*b,list(map(int,numberList)))}"
 
-    elif 'mod' in userInput or 'modulo' in userInput or 'remainder' in userInput or userInput in '%':
-        calculatedResult = int(numberList[0])%int(numberList[1])
+        elif 'sub' in userInput or 'subtract' in userInput or 'subtracted' in userInput or 'subtraction' in userInput or 'minus' in userInput or '-' in userInput:
+            if 'from' in userInput:
+                calculatedResult = f"The result of subtraction is {int(numberList[1]) - int(numberList[0])}"
+            else:
+                calculatedResult = int(numberList[0]) - int(numberList[1])
 
-    elif 'divide' in userInput or 'divided' in userInput or 'division' in userInput or userInput in '/' or 'quotient' in userInput or 'by' in userInput:
-        calculatedResult = int(numberList[0])/int(numberList[1])
+        elif 'mod' in userInput or 'modulo' in userInput or 'remainder' in userInput or '%' in userInput:
+            calculatedResult = f"The remainder we get is {int(numberList[0])%int(numberList[1])}"
 
-    elif 'power' in userInput or 'exponent' in userInput or '**' in userInput:
-        calculatedResult = int(numberList[0])**int(numberList[1])
+        elif 'divide' in userInput or 'divided' in userInput or 'division' in userInput or '/' in userInput or 'quotient' in userInput or 'by' in userInput:
+            calculatedResult = f"The quotient is {int(numberList[0])/int(numberList[1])}"
+
+        elif 'power' in userInput or 'exponent' in userInput or '**' in userInput:
+            calculatedResult = f"The exponential result is {int(numberList[0])**int(numberList[1])}"
+            
+        elif 'bitwise xor' in userInput or 'bitwise ^' in userInput or 'bitwisexor' in userInput or 'bitwise^' in userInput or 'xor' in userInput or '^' in userInput:
+            calculatedResult = f"The bitwise XOR of numbers is {int(numberList[0])^int(numberList[1])}"
+
+        elif 'left shift' in userInput or '< <' in userInput or '<<' in userInput or 'leftshift' in userInput or 'bitwise left shift' in userInput or 'bitwise <<' in userInput or 'bitwiseleftshift' in userInput or 'bitwise<<' in userInput or 'bitwise leftshift' in userInput or 'bitwiseleft shift' in userInput or 'bitwise < <' in userInput or 'bitwise< <' in userInput:
+            calculatedResult = f"The bitwise << of numbers is {int(numberList[0])<<int(numberList[1])}"
+
+        elif 'right shift' in userInput or '> >' in userInput or '>>' in userInput or 'rightshift' in userInput or 'bitwise right shift' in userInput or 'bitwise >>' in userInput or 'bitwiserightshift' in userInput or 'bitwise>>' in userInput or 'bitwise rightshift' in userInput or 'bitwiseright shift' in userInput or 'bitwise > >' in userInput or 'bitwise> >' in userInput:
+            calculatedResult = f"The bitwise >> of numbers is {int(numberList[0])>>int(numberList[1])}"
+
+        elif 'bitwise' in userInput or 'bitwise and' in userInput or 'bitwise &' in userInput or 'bitwiseand' in userInput or 'bitwise&' in userInput:
+            calculatedResult = f"The bitwise & of numbers is {int(numberList[0])&int(numberList[1])}"
 
     return calculatedResult
+
+def expressionCalculationReply(userInput):
+    tokenExpression = re.findall(r'\d+|\*\*|<=|>=|==|!=|[+\-*/%<>]',userInput)
+    expressionString = " ".join(tokenExpression)
+    calculatedResult = ner.evaluate(expressionString)
+    return f"The result of the expression is {calculatedResult.item()}"
