8d3c08cbb46597db9b5e9ea4ec09b0970f156cb9
Pritanshu Srivastava <pritanshusrivastava24880@gmail.com>
2025-06-29 00:46:08 +0530

    Added debug message

--- run.py
+++ run.py
@@ -7,5 +7,6 @@ app = create_app()
 
 if __name__ == '__main__':
     # app.run(debug=True)
+    print("Starting Flask app...")
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
